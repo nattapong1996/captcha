f7836cb778e3b788c19a73641895931fdfe1dd6f
poom.oic <poom.oic@gmail.com>
2018-12-20 15:36:18 +0700

    Captcha case 1,1,1,2 to 1 + Two

--- captcha.go
+++ captcha.go
@@ -4,8 +4,13 @@ package captcha
 func Captcha(no0,no1,no2,no3 int) string{
 
 	if no0 == 1 && no1 == 1 && no2 == 1 && no3 == 1 {
-		return "1" + "+" + "one"
+		return "1" + "+" + "One"
 	}
+
+	if no0 == 1 && no1 == 1 && no2 == 1 && no3 == 2 {
+		return "1" + "+" + "Two"
+	}
+
 	return "Error"
 
 } 
--- captcha_test.go
+++ captcha_test.go
@@ -7,7 +7,15 @@ import (
 
 func TestCaptchaCase1111to1plusOne(t *testing.T)  {
 	result := Captcha(1,1,1,1)
-	experd := "1+one"
+	experd := "1+One"
+	if result == experd {
+		fmt.Println(result)
+	}
+}
+
+func TestCaptchaCase1112to1plusTwo(t *testing.T)  {
+	result := Captcha(1,1,1,1)
+	experd := "1+Two"
 	if result == experd {
 		fmt.Println(result)
 	}
