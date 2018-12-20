bfd23d870a82efa51c9c307fd242d6b5fadb6b17
poom.oic <poom.oic@gmail.com>
2018-12-20 15:06:11 +0700

    Captcha edit case 1,1,1,1 to 1 + One

--- captcha.go
+++ captcha.go
@@ -4,7 +4,7 @@ package captcha
 func Captcha(no0,no1,no2,no3 int) string{
 
 	if no0 == 1 && no1 == 1 && no2 == 1 && no3 == 1 {
-		return "1" + "+" + "one"
+		return "1" + "+" + "One"
 	}
 	return "Error"
 
--- captcha_test.go
+++ captcha_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCaptchaCase1111to1plusOne(t *testing.T)  {
 	result := Captcha(1,1,1,1)
-	experd := "1+one"
+	experd := "1+One"
 	if result == experd {
 		fmt.Println(result)
 	}
